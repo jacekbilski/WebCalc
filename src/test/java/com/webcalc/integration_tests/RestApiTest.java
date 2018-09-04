9d923e33869e49d8f4b295e9322af8253be7c81d
Jacek Bilski <jacek.bilski@gmail.com>
2018-09-04 08:15:27 +0200

    Application starts, listens on 8080, but nobody responds to "/eval".

--- src/test/java/com/webcalc/integration_tests/RestApiTest.java
+++ src/test/java/com/webcalc/integration_tests/RestApiTest.java
@@ -1,10 +1,14 @@
 package com.webcalc.integration_tests;
 
+import com.webcalc.app.WebCalcApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+@SpringBootTest(classes = WebCalcApplication.class,
+    webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class RestApiTest {
 
   @Test
