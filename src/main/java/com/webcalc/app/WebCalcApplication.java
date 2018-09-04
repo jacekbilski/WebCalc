17bd22114955779e3a9006978179f3c5c96ac229
Jacek Bilski <jacek.bilski@gmail.com>
2018-09-04 08:18:40 +0200

    Added controller returning nothing.

--- src/main/java/com/webcalc/app/WebCalcApplication.java
+++ src/main/java/com/webcalc/app/WebCalcApplication.java
@@ -2,8 +2,10 @@ package com.webcalc.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.webcalc")
 public class WebCalcApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/webcalc/calculator/CalculatorController.java
@@ -0,0 +1,12 @@
+package com.webcalc.calculator;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+  @PostMapping("/eval")
+  public void calculate() {
+  }
+}
