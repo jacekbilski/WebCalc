fb831f9224abbac1b8b57f2fc122de2e5f3a89c7
Jacek Bilski <jacek.bilski@gmail.com>
2018-09-04 08:27:05 +0200

    Just created a project at start.spring.io.

---
+++ src/main/java/com/webcalc/app/WebCalcApplication.java
@@ -0,0 +1,12 @@
+package com.webcalc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCalcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebCalcApplication.class, args);
+  }
+}
