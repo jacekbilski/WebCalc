ca112693e4c8851f38b16306b347ff3012ca375b
Jacek Bilski <jacek.bilski@gmail.com>
2018-09-08 10:44:03 +0200

    Calculator as a field.

--- src/main/java/com/webcalc/calculator/CalculatorController.java
+++ src/main/java/com/webcalc/calculator/CalculatorController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
+  private final Calculator calculator;
+
+  public CalculatorController() {
+    calculator = new Calculator();
+  }
+
   @PostMapping("/eval")
   public String calculate() {
-    return new Calculator().eval("");
+    return calculator.eval("");
   }
 }
