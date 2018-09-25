73cf663b08b724ec888ed81e860ef0a1ade195c1
Jacek Bilski <jacek.bilski@gmail.com>
2018-09-25 09:56:10 +0200

    Testing division with rounding from outside.

--- app/src/test/java/com/webcalc/integration_tests/RestApiTest.java
+++ app/src/test/java/com/webcalc/integration_tests/RestApiTest.java
@@ -20,4 +20,21 @@ class RestApiTest {
     .then()
         .body(equalTo("3"));
   }
+
+  @Test
+  void settingMaxFractionDigits() {
+    given()
+        .body("5")
+    .when()
+        .put("/maxFractionDigits")
+    .then()
+        .statusCode(200);
+
+    given()
+        .body("8 7 /")
+    .when()
+        .post("/eval")
+    .then()
+        .body(equalTo("1,14286"));
+  }
 }
