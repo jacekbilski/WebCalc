25a4463e3fd46419d65c3da058eba02f0794b273
Jacek Bilski <jacek.bilski@gmail.com>
2018-10-29 08:55:43 +0100

    Separating sessions test.

--- app/src/test/java/com/webcalc/integration_tests/RestApiTest.java
+++ app/src/test/java/com/webcalc/integration_tests/RestApiTest.java
@@ -1,6 +1,7 @@
 package com.webcalc.integration_tests;
 
 import com.webcalc.app.WebCalcApplication;
+import io.restassured.filter.session.SessionFilter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -31,6 +32,44 @@ class RestApiTest {
         .statusCode(200);
 
     given()
+        .body("8 7 /")
+    .when()
+        .post("/eval")
+    .then()
+        .body(equalTo("1,14286"));
+  }
+
+  @Test
+  void maxFractionDigitsAffectsOnlyCurrentSession() {
+    SessionFilter session1 = new SessionFilter();
+    SessionFilter session2 = new SessionFilter();
+
+    given()
+        .filter(session1)
+        .body("3")
+    .when()
+        .put("/maxFractionDigits")
+    .then()
+        .statusCode(200);
+
+    given()
+        .filter(session2)
+        .body("5")
+    .when()
+        .put("/maxFractionDigits")
+    .then()
+        .statusCode(200);
+
+    given()
+        .filter(session1)
+        .body("8 7 /")
+    .when()
+        .post("/eval")
+    .then()
+        .body(equalTo("1,143"));
+
+    given()
+        .filter(session2)
         .body("8 7 /")
     .when()
         .post("/eval")
