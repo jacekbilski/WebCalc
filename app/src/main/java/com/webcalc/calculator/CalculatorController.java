832c7c47b597434972002d19366bb4d1ee6e9e91
Jacek Bilski <jacek.bilski@gmail.com>
2018-10-30 08:07:03 +0100

    Controller now holds maxFractionDigits in HTTP session.

--- app/src/main/java/com/webcalc/calculator/CalculatorController.java
+++ app/src/main/java/com/webcalc/calculator/CalculatorController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class CalculatorController {
+
+  private static final String MAX_FRACTION_DIGITS = "maxFractionDigits";
 
   private final Calculator calculator;
 
@@ -15,12 +19,12 @@ public class CalculatorController {
   }
 
   @PostMapping("/eval")
-  public String calculate(@RequestBody String body) {
-    return calculator.eval(body);
+  public String calculate(@RequestBody String body, HttpSession session) {
+    return calculator.eval(body, (Integer) session.getAttribute(MAX_FRACTION_DIGITS));
   }
 
   @PutMapping("/maxFractionDigits")
-  public void setMaxFractionDigits(@RequestBody String body) {
-    calculator.setMaxFractionDigits(Integer.parseInt(body));
+  public void setMaxFractionDigits(@RequestBody String body, HttpSession session) {
+    session.setAttribute(MAX_FRACTION_DIGITS, Integer.parseInt(body));
   }
 }
