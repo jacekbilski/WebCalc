f0a95adf7c078982256d649486fed88ffe1f1d6a
Jacek Bilski <jacek@bilski.tech>
2019-05-02 07:43:49 +0200

    REST API test for charging only the user who is doing some calculations.

--- app/src/test/java/com/webcalc/integration_tests/BillingApiShould.java
+++ app/src/test/java/com/webcalc/integration_tests/BillingApiShould.java
@@ -11,13 +11,39 @@ import static org.hamcrest.Matchers.equalTo;
     webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BillingApiShould {
 
-  private static final String DEFAULT_USERNAME = "mmustermann";
-  private static final String DEFAULT_PASSWORD = "9786f3gb4508c2393q7y";
+  private static final String USER1_NAME = "mmustermann";
+  private static final String USER1_PASS = "9786f3gb4508c2393q7y";
+
+  private static final String USER2_NAME = "user2_name";
+  private static final String USER2_PASS = "user2_pass";
 
   @Test
   void returnsZeroWhenNoCalculationsDone() {
     given()
-        .auth().preemptive().basic(DEFAULT_USERNAME, DEFAULT_PASSWORD)
+        .auth().preemptive().basic(USER1_NAME, USER1_PASS)
+    .when()
+        .get("/balance")
+    .then()
+        .body(equalTo("0"));
+  }
+
+  @Test
+  void chargeOnlyUser1_when1IsCalculating() {
+    given()
+        .auth().preemptive().basic(USER1_NAME, USER1_PASS)
+        .body("1 2 +")
+    .when()
+        .post("/eval");
+
+    given()
+        .auth().preemptive().basic(USER1_NAME, USER1_PASS)
+    .when()
+        .get("/balance")
+    .then()
+        .body(equalTo("1"));
+
+    given()
+        .auth().preemptive().basic(USER2_NAME, USER2_PASS)
     .when()
         .get("/balance")
     .then()
